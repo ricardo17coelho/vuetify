6c757301d85e5619e24c87f294664e93cb77c7f9
nekosaur <albert@kaaman.nu>
2020-10-17 16:11:37 +0200

    wip

--- packages/vuetify/src/index.ts
+++ packages/vuetify/src/index.ts
@@ -2,6 +2,7 @@ import * as components from './components'
 import * as framework from './framework'
 
 export * from './components'
+export * from './composables'
 export * from './framework'
 
 export const createVuetify = (options: framework.VuetifyOptions = {}) => {
