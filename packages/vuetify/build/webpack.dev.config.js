b898688c6542f60eda2d5e69bb1f8d28cf389c9b
nekosaur <albert@kaaman.nu>
2020-10-22 17:58:19 +0200

    wip

--- packages/vuetify/build/webpack.dev.config.js
+++ packages/vuetify/build/webpack.dev.config.js
@@ -76,7 +76,7 @@ module.exports = merge(baseWebpackConfig, {
   },
   devServer: {
     contentBase: resolve('../dev'),
-    publicPath: '/dev/',
+    publicPath: '/',
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || '8080',
     disableHostCheck: true
